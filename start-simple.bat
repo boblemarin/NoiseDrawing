7be27a07bc647a0b48e95239adf328c99e140330
boblemarin <emeric@minimal.be>
2024-03-19 14:04:52 +0100

    adding simple batch file for drawing only

---
+++ start-simple.bat
@@ -0,0 +1,24 @@
+@echo off
+
+
+
+start cmd.exe /k "node index.js"
+
+timeout /t 1
+
+rem ==== Killing any instances of chrome
+taskkill /IM "brave.exe">nul 2>&1
+
+timeout /t 2
+
+rem ==== Set local variables
+
+set app_url="http://localhost:3000"
+
+set chrome_path="C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+
+set chrome_flags= --kiosk --disable-pinch --disable-session-crashed-bubble --disable-infobars --overscroll-history-navigation=0 --disable-feature=OutdatedBuildDetector --check-for-update-interval=604800 --disable-web-security --user-data-dir
+
+rem ==== Launch Chrome with app
+
+%chrome_path% %app_url% %chrome_flags%
